49766cedff7aed3935b6e0c96b72c744d246c362
Kavya hb <kavyahb21@gmail.com>
2022-06-11 09:37:10 +0530

    Checkpoint 1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import 'typeface-roboto';
-import registerServiceWorker from './registerServiceWorker';
-import Controller from './screens/Controller';
+import Home from './screens/home/Home';
 
-ReactDOM.render(<Controller />, document.getElementById('root'));
-registerServiceWorker();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+
+    <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='details' element={<Details />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
